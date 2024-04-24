799206be1a20cd739cbab33ef2814e844785d4d6
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2024-04-24 13:23:14 +0300

    as

--- vendor/encore/laravel-admin/src/Controllers/Dashboard.php
+++ vendor/encore/laravel-admin/src/Controllers/Dashboard.php
@@ -174,9 +174,7 @@ class Dashboard
         $disabilityCounts = [];
 
         // Get all people, eager loading their disabilities
-        //$people = Person::where('sex')->where('sex', '<>', '')->get();
-        $people = Person::where('sex')->where('sex', '<>', '')->get();
-        
+        $people = Person::whereNotNull('sex')->where('sex', '<>', '')->get();
 
         foreach ($people as $person) {
             // Determine the person's age group
