6c3dedcdd9f044483e23b21f95fa156d8a967acb
hildah <98084446+HildahN@users.noreply.github.com>
2026-02-23 20:47:50 +0300

    Added more testimonials

--- resources/views/testimonial.blade.php
+++ resources/views/testimonial.blade.php
@@ -43,11 +43,149 @@
        <div class="video-gallery">
                 <div class="video-item">
                     <video controls>
+                        <source src="assets/testimonials/Understanding Disability.mp4" type="video/mp4">
+                        Your browser does not support the video tag.
+                    </video>
+                    <div class="video-caption"><h4>Understanding Disability</h4></div>
+                </div>
+
+                <div class="video-item">
+                    <video controls>
+                        <source src="assets/testimonials/Empowering_PWDs.mp4" type="video/mp4">
+                        Your browser does not support the video tag.
+                    </video>
+                    <div class="video-caption"><h4>Empowering Persons with Disabilities through Digital Literacy</h4></div>
+                </div>
+
+
+                <div class="video-item">
+                    <video controls>
+                        <source src="assets/testimonials/Empowering_Lives.mp4" type="video/mp4">
+                        Your browser does not support the video tag.
+                    </video>
+                    <div class="video-caption"><h4>Empowering Lives,"How Digital Literacy is Transforming Persons with Disabilities"-COU FamilyTV</h4></div>
+                </div>
+
+
+               
+    
+
+
+
+                <div class="video-item">
+                    <video controls>
+                        <source src="assets/testimonials/UNLOCKING_POTENTIAL.mp4" type="video/mp4">
+                        Your browser does not support the video tag.
+                    </video>
+                    <div class="video-caption"><h4>UNLOCKING POTENTIAL VISUALLY IMPAIRED MAN LEARNS SMARTPHONE & COMPUTER SKILLS.-Ict4pwds</h4></div>
+                </div>
+
+
+
+                <div class="video-item">
+                    <video controls>
+                        <source src="assets/testimonials/THE_HOT_SIT.mp4" type="video/mp4">
+                        Your browser does not support the video tag.
+                    </video>
+                    <div class="video-caption"><h4>"Kisaame Samuel",I used to fear looking into a mirror because of my disability,but now that barrier is broken.I'm free.</h4></div>
+                </div>
+
+
+
+                <div class="video-item">
+                    <video controls>
+                        <source src="assets/testimonials/POWER_OF_TECHNOLOGY.mp4" type="video/mp4">
+                        Your browser does not support the video tag.
+                    </video>
+                    <div class="video-caption"><h4>FROM SILENCE TO CONNECTION THE POWER OF TECHNOLOGY FOR THE DEAF - SSEKAJA MANSULU</h4></div>
+                </div>
+
+
+
+                <div class="video-item">
+                    <video controls>
+                        <source src="assets/testimonials/I_HAD_NEVER_USED_A_COMPUTER.mp4" type="video/mp4">
+                        Your browser does not support the video tag.
+                    </video>
+                    <div class="video-caption"><h4>I HAD NEVER USED A COMPUTER IN MY LIFE BUT THANK YOU UCC, EIGHT TECH & NUDIPU.</h4></div>
+                </div>
+
+
+
+                <div class="video-item">
+                    <video controls>
+                        <source src="assets/testimonials/The_Smartphone.mp4" type="video/mp4">
+                        Your browser does not support the video tag.
+                    </video>
+                    <div class="video-caption"><h4>Can Visual Impaired Person Can Use Smartphones Now?</h4></div>
+                </div>
+
+
+
+                <div class="video-item">
+                    <video controls>
+                        <source src="assets/testimonials/Thanks_UCC.mp4" type="video/mp4">
+                        Your browser does not support the video tag.
+                    </video>
+                    <div class="video-caption"><h4>I leant how to design flyers-Viola from Isingiro District</h4></div>
+                </div>
+
+
+
+                 <div class="video-item">
+                    <video controls>
+                        <source src="assets/testimonials/I_was_very_illiterate.mp4" type="video/mp4">
+                        Your browser does not support the video tag.
+                    </video>
+                    <div class="video-caption"><h4>UCC, Eight Tech & NUDIPU you have brought this great program to us thank you so much.</h4></div>
+                </div>
+                
+
+
+                <div class="video-item">
+                    <video controls>
+                        <source src="assets/testimonials/UCC_THANK_YOU.mp4" type="video/mp4">
+                        Your browser does not support the video tag.
+                    </video>
+                    <div class="video-caption"><h4>Thank you for the Digital Skilling now what we need is computers or smartphones</h4></div>
+                </div>
+
+
+                <div class="video-item">
+                    <video controls>
+                        <source src="assets/testimonials/Computer_is_now_important.mp4" type="video/mp4">
+                        Your browser does not support the video tag.
+                    </video>
+                    <div class="video-caption"><h4>I have a computer but did not know how to use thank you for openning my mind</h4></div>
+                </div>
+
+
+                <div class="video-item">
+                    <video controls>
+                        <source src="assets/testimonials/I_can_now_use_Whatsapp.mp4" type="video/mp4">
+                        Your browser does not support the video tag.
+                    </video>
+                    <div class="video-caption"><h4>I can now use WhatsApp,Tiktok & play music with the help of TalkBack. Mr. Malinze Umaru, a visually impaired participant from Kayunga, sharing his Journey into the Digital World after the training</h4></div>
+                </div>
+
+
+
+                <div class="video-item">
+                    <video controls>
+                        <source src="assets/testimonials/Mwebale_Kutusomesa.mp4" type="video/mp4">
+                        Your browser does not support the video tag.
+                    </video>
+                    <div class="video-caption"><h4>This is my first time to touch on the computer.</h4></div>
+                </div>
+        
+                <div class="video-item">
+                    <video controls>
                         <source src="assets/testimonials/ICT4PWD1.mp4" type="video/mp4">
                         Your browser does not support the video tag.
                     </video>
                     <div class="video-caption"><h4>Introduction of National Digital Observatory in Arua</h4></div>
                 </div>
+              
                 <div class="video-item">
                     <video controls>
                         <source src="assets/testimonials/ICT4PWD3.mp4" type="video/mp4">
